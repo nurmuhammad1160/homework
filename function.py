c0a9c003c8c97f63c602799f6499a7993dce607d
Nurmuhammad <oddiybola1160@gmail.com>
2023-10-21 16:10:42 +0500

    salom

---
+++ function.py
@@ -0,0 +1,9 @@
+
+
+
+
+
+def nom(*son, **nomi):
+    return son, nomi
+
+# nom(1,2,3,4,5,6,7,8, ism='Nurmuhammad',famiya='Samadov')
---
+++ main.py
@@ -0,0 +1,5 @@
+from function import nom
+
+malumot = nom(12,13,14,13,'salom','alik', ism='Shuxratjon',familiya='gfrd')
+
+print(malumot)
